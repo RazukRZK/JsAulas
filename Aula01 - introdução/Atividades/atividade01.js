5280d536099052e2d97e4606afc6b08a83c273dd
RazukRZK <146839489+RazukRZK@users.noreply.github.com>
2023-11-21 11:48:59 -0300

    Primeira atividade de Js

--- Aula01 - introdução/Atividades/atividade01.js
+++ Aula01 - introdução/Atividades/atividade01.js
@@ -1,5 +1,14 @@
+// var cliente = Number(prompt("Quantos clientes tem na sua mesa ?"))
+// var valor = Number(prompt("Qual o valor total da conta ?"))
+// var dividido = valor / cliente 
+
+// alert(`Ficou ${dividido} reais pra cada`)
+
+function calculeConta(cliente, valor){
+    let dividido = valor / cliente 
+    alert(`Ficou ${dividido} reais pra cada`)
+}
+
 var cliente = Number(prompt("Quantos clientes tem na sua mesa ?"))
 var valor = Number(prompt("Qual o valor total da conta ?"))
-var dividido = valor / cliente 
-
-alert(`Ficou ${dividido} reais pra cada`)
+calculeConta(cliente,valor)
