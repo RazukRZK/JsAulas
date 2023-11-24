224b0557bf31948082811579a95d67cbdc7525bd
RazukRZK <146839489+RazukRZK@users.noreply.github.com>
2023-11-24 08:58:51 -0300

    Aula 03

--- Aula03 - repetições e array/Atividades/atividade01.js
+++ Aula03 - repetições e array/Atividades/atividade01.js
@@ -1,4 +1,9 @@
+// var filmes = ["Homem de Ferro", "Um maluco no pedaço", "Homem aranha", "Eu a patroa e as crianças", "Vingadores", "Naruto"]
+// filmes.forEach((cadaFilme) => {
+//     console.log(cadaFilme);
+// });
+
 var filmes = ["Homem de Ferro", "Um maluco no pedaço", "Homem aranha", "Eu a patroa e as crianças", "Vingadores", "Naruto"]
-filmes.forEach((cadaFilme) => {
-    console.log(cadaFilme);
-});
+for(var contador = 0; contador < filmes.length; contador++){
+    console.log(filmes[contador])
+}
