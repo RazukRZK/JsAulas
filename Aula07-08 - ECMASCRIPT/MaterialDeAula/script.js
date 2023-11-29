ea897d30341ce37b802d1d903331dd6dd51ff49f
RazukRZK <146839489+RazukRZK@users.noreply.github.com>
2023-11-29 11:50:37 -0300

    Aula 07 - ECMASCRIPT

---
+++ Aula07-08 - ECMASCRIPT/MaterialDeAula/script.js
@@ -0,0 +1,52 @@
+// REPLACE / REPLACEALL
+var frase = "Olá mundo, o mundo é muito legal"
+console.log(frase)
+
+console.log(frase.replace("mundo", "bacana"));
+console.log(frase.replaceAll("mundo", "bacana"));
+
+// SETATRIBUTE
+document.querySelector("span").setAttribute("style", "color: blue;");
+
+// CLASS LIST
+document.querySelector("#classlist").classList.remove("claro");
+document.querySelector("#classlist").classList.add("claro");
+var elemento = document.querySelector("#classlist")
+// contains se contem a classe
+// toggle troca de classe
+if(elemento.classList.contains("claro")){
+    // elemento.classList.toggle("escuro")
+    elemento.classList.replace("claro", "escuro");
+}
+
+// index of / includes
+var frutas = ["Morango", "Banana", "Laranja", "Uva"]
+
+//  includes -> true e false
+console.log(frutas.includes("Abacaxi"));
+//  indexOf -> o valor indice do array (caso não exista, -1)
+console.log(frutas.indexOf("Laranja"));
+
+if(frutas.indexOf("Abacaxi" == -1)){
+    console.log("Não achei")
+}
+
+// SET TIMEOUT  
+function msg(){
+    // PROMISE - SE ASSEMELHA A UMA FUNÇÃO, PORÉM ELE CONFIGURA SUCESSO E ERRO
+    return new Promise ((resolve) =>{
+        setTimeout(() => {
+            console.log("Bom dia!")
+            resolve()
+        }, 2000);
+        
+    })   
+}
+
+async function carregando(){
+    console.log("Iniciou");
+    await msg();
+    console.log('Seja bem vindo');
+}
+
+carregando();
