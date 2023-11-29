80abbbc791a3e7d1fc240a48b480a5ca87bea98d
RazukRZK <146839489+RazukRZK@users.noreply.github.com>
2023-11-29 10:29:02 -0300

    Aula 06 Js

--- Aula06/atividade.js
+++ Aula06/atividade.js
@@ -10,25 +10,39 @@ class Filme {
 
 var btnFilme = document.querySelector("#btnFilme")
 
+var filmes = []
+
 btnFilme.addEventListener("click", cadastrarFilme)
 
 function cadastrarFilme(){
+
+
     let nomeInput = document.querySelector("#nome").value
     let descricaoInput = document.querySelector("#descricao").value
     let dataInput = document.querySelector("#data").value
     let diretorInput = document.querySelector("#diretor").value
     let categoriaInput = document.querySelector("#categoria").value
-    let infoFilmes = document.querySelector("infoFilmes")
 
-    var filme = new Filme(nomeInput, descricaoInput, dataInput, diretorInput, categoriaInput, infoFilmes);
+    filmes.push( new Filme(nomeInput, descricaoInput, dataInput, diretorInput, categoriaInput, infoFilmes));
 
-    infoFilme.innerHTML = `<div class="post">
-    <h3>${filme.nome}</h3>
-    <p>Descrição : ${filme.descricao}</p>
-    <p>Data : ${filme.data}</p>
-    <p>Diretor : ${filme.diretor}</p>
-    <p>Categoria : ${filme.categoria}</p>
-    </div>`
+    
+    listar()
 
 }
+
+function listar(){
+    let infoFilmes = document.querySelector("#infoFilmes")
+
+    infoFilmes.innerHTML = "";
+
+    filmes.forEach((elemento) =>{
+        infoFilmes.innerHTML += `<div class="post">
+        <h3>${elemento.nome}</h3>
+        <p>Descrição : ${elemento.descricao}</p>
+        <p>Data : ${elemento.data}</p>
+        <p>Diretor : ${elemento.diretor}</p>
+        <p>Categoria : ${elemento.categoria}</p>
+        </div>`
+    });
+} 
 
